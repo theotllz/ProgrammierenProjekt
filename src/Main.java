66ec8914482bdaeb22cfda7f38b3badddb18f879
theodortelliez <theodor.telliez@gmail.com>
2024-06-13 11:46:06 +0200

    methode auto eingebaut

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
         System.out.println("theos Muddern fickt sich hart");
         System.out.println("Lauritz fickt sich hart(in betriebssyteme)");
         System.out.println("Harry Muddern fickt sich hart in ihren fetten anus");
+        System.out.println("Nochmal probieren");
+        System.out.println(auto("bumsen");
     }
 
-    public int auto(String input) {
+    public static int auto(String input) {
         int length = input.length();
         return length;
     }
