9f677ced693907a22c0d46bbb33119de8bf72d14
theodortelliez <theodor.telliez@gmail.com>
2024-06-13 11:42:41 +0200

    methode auto erstellt

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,9 @@ public class Main {
         System.out.println("theos Muddern fickt sich hart");
         System.out.println("Lauritz fickt sich hart(in betriebssyteme)");
     }
+
+    public int auto(String input) {
+        int length = input.length();
+        return length;
+    }
 }
