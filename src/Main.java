eb03f2f95446317ac91812ffa147d281280cd86a
theodortelliez <theodor.telliez@gmail.com>
2024-06-13 11:38:59 +0200

    LAURITZ DEN HURENSOHN HAB ICH VERÄNDERT

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("theos Muddern fickt sich hart");
-
+        System.out.println("Lauritz fickt sich hart(in betriebssyteme)");
     }
 }
